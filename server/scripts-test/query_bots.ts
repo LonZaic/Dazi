fd9589286666fb411596c98e667795eacbb8e88d
Sylvanli <15699488+sylvanli@user.noreply.gitee.com>
2026-07-03 09:13:11 +0800

    Remove test files from repo

--- server/scripts-test/query_bots.ts
+++
@@ -1,3 +0,0 @@
-import { getDB } from './src/db/index.js'
-const r = getDB().prepare(`SELECT id, username, display_name FROM users WHERE username IN ('alice_intj','bob_enfp','carol_isfj','david_entp')`).all()
-console.log(JSON.stringify(r, null, 2))
